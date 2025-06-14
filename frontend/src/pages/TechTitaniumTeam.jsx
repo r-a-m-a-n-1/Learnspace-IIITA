63f89949c83129f99a488153c56f01f63b69a4ca
r-a-m-a-n-1 <ramangautam0037@gmail.com>
2025-06-14 21:50:32 +0530

    new changes

--- frontend/src/pages/TechTitaniumTeam.jsx
+++ frontend/src/pages/TechTitaniumTeam.jsx
@@ -9,7 +9,7 @@ const teamMembers = [
   {
     name: 'Abhishek',
     role: 'Data Curator Frontend Developer & Data Analyst,"contributed to LearnSpace by integrating educational video content, analyzing usage data, and designing key frontend components for an interactive learning experience."',
-    imgSrc: '/abhishek.jpg',
+    imgSrc: '/a.jpg',
   },
 ];
 
