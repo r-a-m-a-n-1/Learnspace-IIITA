6dca2668206ad1fc32f4e8e59becd08545407398
r-a-m-a-n-1 <ramangautam0037@gmail.com>
2025-06-19 20:35:21 +0530

    logo added

--- frontend/src/pages/Sem3/IT_BI/Sem3_itbi_LectureNotes/Sem3_itbi_LectureNotes.jsx
+++ frontend/src/pages/Sem3/IT_BI/Sem3_itbi_LectureNotes/Sem3_itbi_LectureNotes.jsx
@@ -16,7 +16,7 @@ const Sem3_itbi_LectureNotes = () => {
             to="/sem3/itbi/foundation-fintech-notes"
             className="bg-gradient-to-br from-green-400 to-green-500 text-white shadow-xl rounded-xl w-full max-w-xs h-60 flex flex-col justify-center items-center px-4 transition-all duration-300 transform hover:scale-105 hover:rotate-3 hover:shadow-2xl"
           >
-            <img src="/en_py.png" alt="Foundation Of Fintech" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/fin.png" alt="Foundation Of Fintech" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-lg sm:text-xl font-bold text-center">Foundation Of Fintech</h2>
           </Link>
 
@@ -25,7 +25,7 @@ const Sem3_itbi_LectureNotes = () => {
             to="/sem3/itbi/indian-economics-notes"
             className="bg-gradient-to-br from-red-400 to-red-500 text-white shadow-xl rounded-xl w-full max-w-xs h-60 flex flex-col justify-center items-center px-4 transition-all duration-300 transform hover:scale-105 hover:rotate-3 hover:shadow-2xl"
           >
-            <img src="/fee.png" alt="Indian Economics" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/eco.png" alt="Indian Economics" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-lg sm:text-xl font-bold text-center">Indian Economics</h2>
           </Link>
 
@@ -34,7 +34,7 @@ const Sem3_itbi_LectureNotes = () => {
             to="/sem3/itbi/introduction-finance-notes"
             className="bg-gradient-to-br from-blue-400 to-blue-500 text-white shadow-xl rounded-xl w-full max-w-xs h-60 flex flex-col justify-center items-center px-4 transition-all duration-300 transform hover:scale-105 hover:rotate-3 hover:shadow-2xl"
           >
-            <img src="/linear_algebra.png" alt="Introduction To Finance" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/intro.png" alt="Introduction To Finance" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-lg sm:text-xl font-bold text-center">Introduction To Finance</h2>
           </Link>
 
@@ -43,7 +43,7 @@ const Sem3_itbi_LectureNotes = () => {
             to="/sem3/itbi/oopm-notes"
             className="bg-gradient-to-br from-purple-400 to-purple-500 text-white shadow-xl rounded-xl w-full max-w-xs h-60 flex flex-col justify-center items-center px-4 transition-all duration-300 transform hover:scale-105 hover:rotate-3 hover:shadow-2xl"
           >
-            <img src="/psp.png" alt="Object Oriented Methodologies" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/op1.png" alt="Object Oriented Methodologies" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-lg sm:text-xl font-bold text-center">Object Oriented Methodologies</h2>
           </Link>
 
@@ -52,7 +52,7 @@ const Sem3_itbi_LectureNotes = () => {
             to="/sem3/itbi/operating-system-notes"
             className="bg-gradient-to-br from-orange-400 to-orange-500 text-white shadow-xl rounded-xl w-full max-w-xs h-60 flex flex-col justify-center items-center px-4 transition-all duration-300 transform hover:scale-105 hover:rotate-3 hover:shadow-2xl"
           >
-            <img src="/com_skill.png" alt="Operating System" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/operating.png" alt="Operating System" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-lg sm:text-xl font-bold text-center">Operating System</h2>
           </Link>
 
@@ -61,7 +61,7 @@ const Sem3_itbi_LectureNotes = () => {
             to="/sem3/itbi/prayas-notes"
             className="bg-gradient-to-br from-yellow-400 to-yellow-500 text-white shadow-xl rounded-xl w-full max-w-xs h-60 flex flex-col justify-center items-center px-4 transition-all duration-300 transform hover:scale-105 hover:rotate-3 hover:shadow-2xl"
           >
-            <img src="/ethics.png" alt="Prayas" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/pray.png" alt="Prayas" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-lg sm:text-xl font-bold text-center">Prayas</h2>
           </Link>
 
@@ -70,7 +70,7 @@ const Sem3_itbi_LectureNotes = () => {
             to="/sem3/itbi/probability-statistics-notes"
             className="bg-gradient-to-br from-pink-400 to-pink-500 text-white shadow-xl rounded-xl w-full max-w-xs h-60 flex flex-col justify-center items-center px-4 transition-all duration-300 transform hover:scale-105 hover:rotate-3 hover:shadow-2xl"
           >
-            <img src="/ethics.png" alt="Probability And Statistics" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/pns.png" alt="Probability And Statistics" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-lg sm:text-xl font-bold text-center">Probability And Statistics</h2>
           </Link>
 
@@ -79,7 +79,7 @@ const Sem3_itbi_LectureNotes = () => {
             to="/sem3/itbi/software-engineering-notes"
             className="bg-gradient-to-br from-cyan-400 to-cyan-500 text-white shadow-xl rounded-xl w-full max-w-xs h-60 flex flex-col justify-center items-center px-4 transition-all duration-300 transform hover:scale-105 hover:rotate-3 hover:shadow-2xl"
           >
-            <img src="/ethics.png" alt="Software Engineering" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/soft.png" alt="Software Engineering" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-lg sm:text-xl font-bold text-center">Software Engineering</h2>
           </Link>
 
--- frontend/src/pages/Sem3/IT_BI/Sem3_itbi_VedioPlaylist/Sem3_itbi_Vedio.jsx
+++ frontend/src/pages/Sem3/IT_BI/Sem3_itbi_VedioPlaylist/Sem3_itbi_Vedio.jsx
@@ -13,7 +13,7 @@ const Sem3_itbi_Video = () => {
             to="/sem3/itbi/foundation-fintech-videos"
             className="cursor-pointer bg-gradient-to-br from-green-400 to-green-500 text-white shadow-xl rounded-xl w-60 h-60 flex flex-col justify-center items-center transition-all duration-300 transform hover:scale-110 hover:rotate-6 hover:shadow-2xl"
           >
-            <img src="/en_py.png" alt="Foundation Of Fintech" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/fin.png" alt="Foundation Of Fintech" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-xl font-bold text-center px-2">Foundation Of Fintech</h2>
           </Link>
 
@@ -22,7 +22,7 @@ const Sem3_itbi_Video = () => {
             to="/sem3/itbi/indian-economics-videos"
             className="cursor-pointer bg-gradient-to-br from-red-400 to-red-500 text-white shadow-xl rounded-xl w-60 h-60 flex flex-col justify-center items-center transition-all duration-300 transform hover:scale-110 hover:rotate-6 hover:shadow-2xl"
           >
-            <img src="/fee.png" alt="Indian Economics" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/eco.png" alt="Indian Economics" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-xl font-bold text-center px-2">Indian Economics</h2>
           </Link>
 
@@ -31,7 +31,7 @@ const Sem3_itbi_Video = () => {
             to="/sem3/itbi/introduction-finance-videos"
             className="cursor-pointer bg-gradient-to-br from-blue-400 to-blue-500 text-white shadow-xl rounded-xl w-60 h-60 flex flex-col justify-center items-center transition-all duration-300 transform hover:scale-110 hover:rotate-6 hover:shadow-2xl"
           >
-            <img src="/linear_algebra.png" alt="Introduction To Finance" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/intro.png" alt="Introduction To Finance" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-xl font-bold text-center px-2">Introduction To Finance</h2>
           </Link>
 
@@ -40,7 +40,7 @@ const Sem3_itbi_Video = () => {
             to="/sem3/itbi/oopm-videos"
             className="cursor-pointer bg-gradient-to-br from-purple-400 to-purple-500 text-white shadow-xl rounded-xl w-60 h-60 flex flex-col justify-center items-center transition-all duration-300 transform hover:scale-110 hover:rotate-6 hover:shadow-2xl"
           >
-            <img src="/psp.png" alt="Object Oriented Methodologies" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/op1.png" alt="Object Oriented Methodologies" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-xl font-bold text-center px-2">Object Oriented Methodologies</h2>
           </Link>
 
@@ -49,7 +49,7 @@ const Sem3_itbi_Video = () => {
             to="/sem3/itbi/operating-system-videos"
             className="cursor-pointer bg-gradient-to-br from-orange-400 to-orange-500 text-white shadow-xl rounded-xl w-60 h-60 flex flex-col justify-center items-center transition-all duration-300 transform hover:scale-110 hover:rotate-6 hover:shadow-2xl"
           >
-            <img src="/com_skill.png" alt="Operating System" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/operating.png" alt="Operating System" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-xl font-bold text-center px-2">Operating System</h2>
           </Link>
 
@@ -58,7 +58,7 @@ const Sem3_itbi_Video = () => {
             to="/sem3/itbi/prayas-videos"
             className="cursor-pointer bg-gradient-to-br from-yellow-400 to-yellow-500 text-white shadow-xl rounded-xl w-60 h-60 flex flex-col justify-center items-center transition-all duration-300 transform hover:scale-110 hover:rotate-6 hover:shadow-2xl"
           >
-            <img src="/ethics.png" alt="Prayas" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/pray.png" alt="Prayas" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-xl font-bold text-center px-2">Prayas</h2>
           </Link>
 
@@ -67,7 +67,7 @@ const Sem3_itbi_Video = () => {
             to="/sem3/itbi/probability-statistics-videos"
             className="cursor-pointer bg-gradient-to-br from-pink-400 to-pink-500 text-white shadow-xl rounded-xl w-60 h-60 flex flex-col justify-center items-center transition-all duration-300 transform hover:scale-110 hover:rotate-6 hover:shadow-2xl"
           >
-            <img src="/ethics.png" alt="Probability And Statistics" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/pns.png" alt="Probability And Statistics" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-xl font-bold text-center px-2">Probability And Statistics</h2>
           </Link>
 
@@ -76,7 +76,7 @@ const Sem3_itbi_Video = () => {
             to="/sem3/itbi/software-engineering-videos"
             className="cursor-pointer bg-gradient-to-br from-cyan-400 to-cyan-500 text-white shadow-xl rounded-xl w-60 h-60 flex flex-col justify-center items-center transition-all duration-300 transform hover:scale-110 hover:rotate-6 hover:shadow-2xl"
           >
-            <img src="/ethics.png" alt="Software Engineering" className="w-16 h-16 mb-4 animate-bounce" />
+            <img src="/soft.png" alt="Software Engineering" className="w-16 h-16 mb-4 animate-bounce" />
             <h2 className="text-xl font-bold text-center px-2">Software Engineering</h2>
           </Link>
 
